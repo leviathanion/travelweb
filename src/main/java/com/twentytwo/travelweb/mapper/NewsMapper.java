e18ee338450d52964e3eb1bd43c777efcae692c9
Konna <1051027793@qq.com>
2020-09-21 15:21:00 +0800

    新闻审核bug修改

--- src/main/java/com/twentytwo/travelweb/mapper/NewsMapper.java
+++ src/main/java/com/twentytwo/travelweb/mapper/NewsMapper.java
@@ -22,7 +22,7 @@ public interface NewsMapper {
     @Delete("delete from news_info where news_id=#{news_id}")
     Integer deleteNews(Integer news_id);
 
-    @Update("update news_info set news_title=#{news_title},news_content=#{news_content},news_img_url=#{news_img_url},news_product=#{news_product} where news_id=#{news_id}")
+    @Update("update news_info set news_title=#{news_title},news_content=#{news_content},news_img_url=#{news_img_url},news_product=#{news_product},news_status=#{news_status} where news_id=#{news_id}")
     Integer updateNews(News news);
 
     @Select("select * from news_info where news_id=#{news_id}")
@@ -37,15 +37,15 @@ public interface NewsMapper {
     @Select("select news_info.*,product_info.product_name,com_info.com_name from news_info,product_info,com_info where news_info.news_product=product_info.product_id and product_info.product_com=com_info.com_id and news_info.news_id=#{news_id}")
     NewsInfo getNewsInfoByID(int news_id);
 
-    @Select("select count(*) from news_info")
+    @Select("select count(*) from news_info where news_status is true")
     int findTotalCount();
 
-    @Select("select * from news_info limit #{start},#{pageSize}")
+    @Select("select * from news_info where news_status is true limit #{start},#{pageSize}")
     List<News> findByPage(@Param("start") int start, @Param("pageSize") int pageSize);
 
-    @Select("select count(*) from news_info where news_title like #{category_name}")
+    @Select("select count(*) from news_info where news_status is true and news_title like #{category_name}")
     int findTotalCount1(String category_name);
 
-    @Select("select * from news_info where news_title like #{category_name} limit #{start},#{pageSize}")
+    @Select("select * from news_info where news_status is true and news_title like #{category_name} limit #{start},#{pageSize}")
     List<News> findByPage1(@Param("start") int start, @Param("pageSize") int pageSize,@Param("category_name") String category_name);
 }
