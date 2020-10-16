128b49b251c675dea8b2e0e43a10eea138a46198
Konna <1051027793@qq.com>
2020-10-16 12:17:10 +0800

    打包成war包

--- src/main/java/com/twentytwo/travelweb/TravelwebApplication.java
+++ src/main/java/com/twentytwo/travelweb/TravelwebApplication.java
@@ -2,12 +2,18 @@ package com.twentytwo.travelweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TravelwebApplication {
+public class TravelwebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelwebApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TravelwebApplication.class);
+    }
 }
